f8333e3dd9040817936b3fd326ba7171fd351df0
Godday Okoduwa <goddayokos@gmail.com>
2021-08-07 02:32:33 +0100

    Initial project setup and testing

---
+++ src/main/kotlin/dev/decagon/godday/plugins/Routing.kt
@@ -0,0 +1,21 @@
+package dev.decagon.godday.plugins
+
+import io.ktor.routing.*
+import io.ktor.http.*
+import io.ktor.application.*
+import io.ktor.response.*
+import io.ktor.request.*
+
+fun Application.configureRouting() {
+    // Starting point for a Ktor app:
+    routing {
+        get("/") {
+            call.respondText("Hello World!")
+        }
+
+        get("/hello") {
+            call.respondText("hello, ktor")
+        }
+    }
+
+}
