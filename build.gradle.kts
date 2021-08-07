a96f5fb6c12aa89d4a085310b33663c010cd367c
Godday Okoduwa <goddayokos@gmail.com>
2021-08-07 04:19:50 +0100

    Added the DefaultHeaders, StatusPages and moshi dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 val ktor_version: String by project
 val kotlin_version: String by project
 val logback_version: String by project
+val moshi_version: String by project
 
 plugins {
     application
@@ -21,6 +22,7 @@ dependencies {
     implementation("io.ktor:ktor-server-core:$ktor_version")
     implementation("io.ktor:ktor-server-netty:$ktor_version")
     implementation("ch.qos.logback:logback-classic:$logback_version")
+    implementation("com.ryanharter.ktor:ktor-moshi:$moshi_version")
     testImplementation("io.ktor:ktor-server-tests:$ktor_version")
     testImplementation("org.jetbrains.kotlin:kotlin-test:$kotlin_version")
 }
--- src/main/kotlin/dev/decagon/godday/Application.kt
+++ src/main/kotlin/dev/decagon/godday/Application.kt
@@ -3,9 +3,26 @@ package dev.decagon.godday
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import dev.decagon.godday.plugins.*
+import io.ktor.application.*
+import io.ktor.features.*
+import io.ktor.http.*
+import io.ktor.response.*
 
 fun main() {
     embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
+
+        // DefaultHeaders are used to set HTTP Headers for responses
+        install(DefaultHeaders)
+
+        // Used to respond to failures or errors
+        install(StatusPages) {
+            exception<Throwable> { e ->
+                call.respondText(e.localizedMessage,
+                 ContentType.Text.Plain, HttpStatusCode.InternalServerError
+                )
+            }
+        }
+
         configureRouting()
     }.start(wait = true)
 }
