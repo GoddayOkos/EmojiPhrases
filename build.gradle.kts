0840e77794f3c4e8c14d6c9b77705b09cc7484da
Godday Okoduwa <goddayokos@gmail.com>
2021-08-08 16:32:20 +0100

    Added H2, expose and HikariCP libraries to the project

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,9 @@
 val ktor_version: String by project
 val kotlin_version: String by project
 val logback_version: String by project
+val exposed_version: String by project
+val h2_version: String by project
+val hikaricp_version: String by project
 
 plugins {
     application
@@ -25,6 +28,9 @@ dependencies {
     implementation("io.ktor:ktor-freemarker:$ktor_version")
     implementation("io.ktor:ktor-auth:$ktor_version")
     implementation("io.ktor:ktor-locations:$ktor_version")
+    implementation("org.jetbrains.exposed:exposed:$exposed_version")
+    implementation("com.h2database:h2:$h2_version")
+    implementation("com.zaxxer:HikariCP:$hikaricp_version")
     testImplementation("io.ktor:ktor-server-tests:$ktor_version")
     testImplementation("org.jetbrains.kotlin:kotlin-test:$kotlin_version")
 }
