f5fa84b67c529f93f41064510ff1a64236f169cf
Godday Okoduwa <goddayokos@gmail.com>
2021-08-08 18:23:14 +0100

    Created a H2 database and connect to it via Hikari

---
+++ src/main/kotlin/dev/decagon/godday/repository/DatabaseFactory.kt
@@ -0,0 +1,38 @@
+package dev.decagon.godday.repository
+
+import com.zaxxer.hikari.*
+import dev.decagon.godday.model.*
+import org.jetbrains.exposed.sql.*
+import org.jetbrains.exposed.sql.transactions.*
+
+object DatabaseFactory {
+
+    fun init() {
+        Database.connect(hikari())
+
+        transaction {
+            SchemaUtils.create(EmojiPhrases)
+
+            EmojiPhrases.insert {
+                it[emoji] = "e1"
+                it[phrase] = "p1"
+            }
+            EmojiPhrases.insert {
+                it[emoji] = "e2"
+                it[phrase] = "p2"
+            }
+        }
+    }
+
+    private fun hikari(): HikariDataSource {
+        val config = HikariConfig().apply {
+            driverClassName = "org.h2.Driver"
+            jdbcUrl = "jdbc:h2:mem:test"
+            maximumPoolSize = 3
+            isAutoCommit = false
+            transactionIsolation = "TRANSACTION_REPEATABLE_READ"
+            validate()
+        }
+        return HikariDataSource(config)
+    }
+}
